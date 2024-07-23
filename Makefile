4d5a4ff19f8abad02d010884578b5dc2ca8cae0f
pes18fan <pes18fan@disr.it>
2024-07-23 15:25:00 +0545

    refactor: remove deps from repo and add to build pipeline instead

--- Makefile
+++ Makefile
@@ -1,5 +1,24 @@
-all:
-	clang++ -std=c++17 -lpthread -lm -ldl -o fonograf main.cpp fonograf.cpp ui.cpp
+VENDOR_DIR = vendor
+MINIAUDIO_HEADER = $(VENDOR_DIR)/miniaudio.h
+ROGUEUTIL_HEADER = $(VENDOR_DIR)/rogueutil.h
+MINIAUDIO_URL = https://raw.githubusercontent.com/mackron/miniaudio/master/miniaudio.h
+ROGUEUTIL_URL = https://raw.githubusercontent.com/sakhmatd/rogueutil/master/rogueutil.h
 
+all: fonograf
+
+$(VENDOR_DIR):
+	mkdir -p $(VENDOR_DIR)
+
+$(MINIAUDIO_HEADER): | $(VENDOR_DIR)
+	curl -o $(MINIAUDIO_HEADER) $(MINIAUDIO_URL)
+
+$(ROGUEUTIL_HEADER): | $(VENDOR_DIR)
+	curl -o $(ROGUEUTIL_HEADER) $(ROGUEUTIL_URL)
+
+all: main.cpp fonograf.cpp ui.cpp $(MINIAUDIO_HEADER) $(ROGUEUTIL_HEADER)
+	clang++ -std=c++17 -ladvapi32 -o fonograf main.cpp fonograf.cpp ui.cpp
+
+.PHONY: clean
 clean:
 	rm -f fonograf
+	rm -rf $(VENDOR_DIR)
--- build.bat
+++ build.bat
@@ -1,6 +1,36 @@
 @echo off
 setlocal enabledelayedexpansion
 
+set "vendor_dir=vendor"
+set "miniaudio_url=https://raw.githubusercontent.com/mackron/miniaudio/master/miniaudio.h"
+set "rogueutil_url=https://raw.githubusercontent.com/sakhmatd/rogueutil/master/rogueutil.h"
+
+:: Check if vendor dir is present
+if not exist "%vendor_dir%" (
+    mkdir "%vendor_dir%"
+)
+
+:: Check for existence of miniaudio and rogueutil
+if not exist "%vendor_dir%\miniaudio.h" (
+    echo Downloading miniaudio.h...
+    curl -o "%vendor_dir%\miniaudio.h" "%miniaudio_url%"
+    if %errorlevel% neq 0 (
+        echo Failed to get miniaudio.h
+        exit /b 1
+    )
+)
+
+if not exist "%vendor_dir%\rogueutil.h" (
+    echo Downloading rogueutil.h...
+    curl -o "%vendor_dir%\rogueutil.h" "%rogueutil_url%"
+    if %errorlevel% neq 0 (
+        echo Failed to get rogueutil.h
+        exit /b 1
+    )
+)
+
+:: Compile
+echo Compiling...
 clang++ -std=c++17 -ladvapi32 -o fonograf.exe main.cpp fonograf.cpp ui.cpp
  
 if %errorlevel% neq 0 (
